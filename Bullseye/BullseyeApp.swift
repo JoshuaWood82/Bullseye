d90e9ad69dc2af887cfc3ee32f436cb0cc7dd93a
Joshua Wood <jwood39@live.spcollege.edu>
2021-01-13 18:17:00 -0500

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Joshua Wood on 1/13/21.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
